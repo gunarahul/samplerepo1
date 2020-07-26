27aea882ffca240143ff18047d19ec853e2a6dc4
luharg <gunarahul@gmail.com>
2020-07-26 18:17:20 -0400

    Update seleniumtest.java

--- src/test/java/devops_cert_project/devops_phpwebsite_project_2019/seleniumtest.java
+++ src/test/java/devops_cert_project/devops_phpwebsite_project_2019/seleniumtest.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import static org.testng.Assert.assertEquals;
+import org.openqa.selenium.By;
 
 public class seleniumtest {
 	
@@ -29,15 +33,40 @@ public class seleniumtest {
 	
 	@Test
 	public static void pageCheck() {
-		
-		String page_title=driver.getTitle();
-		System.out.println(page_title);
-		if(page_title.equalsIgnoreCase("FINVIZ.com - Stock Screener")) {
-			System.out.println("Title Check passed");
-		}
-		else
-			System.out.println("Incorrect page loaded");
-		
+		 String x =driver.getTitle();
+	  assertEquals(x, "Home | Simple PHP Website");
+	  System.out.println(x);
+	  
+	// driver.findElement(By.id("id")).sendKeys("About Us");
+	  driver.findElement(By.id("About Us")).click();
+		Thread.sleep(2000);
+	  
+	//String y=   driver.findElement(By.xpath("//*[@id=\"PID-ab2-pg\"]/text()[2]")).getText();
+	  //System.out.println(y);
+	//*[@id="PID-ab2-pg"]/text()[2]
+	  
+	  boolean text= driver.getPageSource().contains("Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry. ");
+	  System.out.println(text);
+	  
+	  //String expectedText= "Lorem Ipsum Dipsum is simply dummy text of the printing and typesetting industry.";
+        //System.out.println(expectedText);
+        
+      
+        boolean flag = false;
+        if (text)
+        	{
+        	flag = true;
+        	System.out.println("the website is deployed successfully  and it's running fine");
+        	}
+        
+        Assert.assertTrue(flag, "Looks like there is some issue,please check");
 	}
-
+	
+	
+  @AfterClass
+  public void afterClass() {
+	  
+	  driver.close();
+	  
+  }
 }
