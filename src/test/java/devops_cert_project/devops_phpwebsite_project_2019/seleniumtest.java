20f1016bf4cce837318cba10751d921a4898942f
luharg <gunarahul@gmail.com>
2020-07-26 18:27:58 -0400

    Update seleniumtest.java

--- src/test/java/devops_cert_project/devops_phpwebsite_project_2019/seleniumtest.java
+++ src/test/java/devops_cert_project/devops_phpwebsite_project_2019/seleniumtest.java
@@ -34,7 +34,7 @@ public class seleniumtest {
 	@Test
 	public static void pageCheck() throws InterruptedException  {
 		 String x =driver.getTitle();
-	  assertEquals(x, "Homes | Simple PHP Website");
+	  assertEquals(x, "Home | Simple PHP Website");
 	  System.out.println(x);
 
 	}
